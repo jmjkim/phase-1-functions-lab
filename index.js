b58275290b97915b36736bfef3b4eafb41f5bf2c
Jongmin Jack Kim <jmjkim00@icloud.com>
2022-02-01 18:43:25 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(street) {
+    const distance = Math.abs(street - 42); 
+
+    return distance;
+}
+
+function distanceFromHqInFeet(street) {
+    const distanceInFeet = distanceFromHqInBlocks(street) * 264;
+
+    return distanceInFeet;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    const travelledDistance = Math.abs(start - destination) * 264;
+    
+    return travelledDistance;
+}
+
+function calculatesFarePrice(start, destination) {
+    const travelledDistance = Math.abs(start - destination) * 264;
+
+    if (travelledDistance <= 400) {
+        return 0;
+    }
+    if (travelledDistance > 400 && travelledDistance < 2000) {
+        return ((travelledDistance - 400) * 2) / 100;
+    }
+    if (travelledDistance > 2000 && travelledDistance < 2500) {
+        return 25;
+    }
+    if (travelledDistance > 2500) {
+        return 'cannot travel that far';
+    }
+}
